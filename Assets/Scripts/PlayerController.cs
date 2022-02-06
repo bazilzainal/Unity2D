e76a92262c7c3d7bb1b5021c01f573067e434b40
Bazil Zainal <bazilzainal@gmail.com>
2022-02-06 21:39:57 +0800

    Add rotational speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,20 +5,34 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public int moveSpeed = 10;
+    [SerializeField] float moveSpeed = 0.5f;
+    [SerializeField] float turnSpeed = 1f;
+    private SpriteRenderer test;
+
     // Start is called before the first frame update
     void Start()
     {
+        test = GetComponent<SpriteRenderer>();
+        test.color = Color.cyan;
+
         Debug.Log("Testing");
+
     }
 
+    private void Update()
+    {
+
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
-        float horizontalInput = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
-        float verticalInput = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+        // float horizontalInput = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+        // float verticalInput = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+        // transform.Translate(new Vector2(horizontalInput, verticalInput));
 
-        transform.Translate(new Vector2(horizontalInput, verticalInput));
+        transform.Translate(0, moveSpeed, 0);
+        transform.Rotate(0, 0, turnSpeed);
+
     }
 
 }
