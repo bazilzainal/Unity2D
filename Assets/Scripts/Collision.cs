d7ddd4a031a5b3832f978bb5bea4d177dfbf99e6
Bazil Zainal <bazilzainal@gmail.com>
2022-02-07 22:28:56 +0800

    Add Assets and other stuff

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -7,4 +7,12 @@ public class Collision : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Hit");        
     }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Triggered");
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        Debug.Log("Exit");
+    }
 }
