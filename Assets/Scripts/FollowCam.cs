cd79cabe783243f01fb213349489c4a3c31de1f2
Bazil Z <bazilzainal@gmail.com>
2022-02-07 22:51:45 +0800

    Add FollowCamera, create basic level

---
+++ Assets/Scripts/FollowCam.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+
+    [SerializeField] GameObject player;
+    // Camera should follow player
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = player.transform.position + new Vector3(0,0,-10);        
+    }
+}
