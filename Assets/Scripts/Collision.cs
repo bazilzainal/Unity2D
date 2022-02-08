fa704e38558b63f9f0db7bf7bfc678e97281c93e
Bazil Zainal <bazilzainal@gmail.com>
2022-02-08 10:49:09 +0800

    Add ball-kick collision script

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+    public int kickForce = 10;
+
     private void OnCollisionEnter2D(Collision2D other) {
-        Debug.Log("Hit");        
+        Debug.Log("Hit");    
+        Vector3 direction = (other.transform.position - transform.position).normalized;
+        GetComponent<Rigidbody2D>().AddForce(-direction * kickForce, ForceMode2D.Impulse);
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
